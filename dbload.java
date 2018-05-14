e8a96d1fb3372d970e44ecc16d9d616a5bdf7eaf
s3640574 <37169921+s3640574@users.noreply.github.com>
2018-05-15 04:28:15 +1000

    Fixed some issues

--- dbload.java
+++ dbload.java
@@ -1,152 +1,172 @@
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class dbload{
+	static int pageSize;
+	//static String dataFile = "C:\\Users\\Administrator\\eclipse-workspace\\db2\\src\\BUSINESS_NAMES_201803.csv";
+	static String dataFile = "/home/ec2-user/db2pt3/BUSINESS_NAMES_201803.csv";
 
 
-public class dbload {
-	
+	public static void main (String[] args){
 
-	public static int count=0;
-	public static String pageSize, searchQuery;
-    public static byte[] REGISTER_NAME, BN_NAME, BN_STATUS, BN_REG_DT, BN_CANCEL_DT, BN_RENEW_DT, BN_STATE_NUM, BN_STATE_OF_REG, BN_ABN = null;
-    public static DataOutputStream hFile = null;
-	
-    public static void main(String[] args) {
-    	
-        //String csvFile = "C:\\Users\\Victor Lee\\eclipse-dbs1\\DBHeapFile\\src\\BUSINESS_NAMES_201803.csv";
-        //String csvFile = "C:\\Users\\Victor Lee\\eclipse-dbs1\\DBHeapFile\\src\\testline.txt";
-    	String csvFile = "/home/ec2-user/javadbs1/BUSINESS_NAMES_201803.csv";
-        BufferedReader br = null;
-        String line = "";
-        String cvsSplitBy = "\t";
-        
-        if(args[0].compareTo("-p") == 0) {
-        	pageSize = args[1];
-        	int pageSizeINT = Integer.parseInt(pageSize);
-        	long startTime = System.nanoTime();
-	        try {
-	
-	            br = new BufferedReader(new FileReader(csvFile));
-	            hFile = new DataOutputStream(new FileOutputStream("heap." + pageSize));
+			if(args[0].equals("-p")){
+				pageSize = Integer.parseInt(args[1]);			
+			}
+			else {
+				System.out.println("Error!.");
+				System.exit(0);
+			}
+		
+		BufferedReader br = null;
+		DataOutputStream os = null;
+		String csvDelimiter = "\t";
+		String line;
+		String BN_NAME, BN_STATUS, BN_REG_DT, BN_CANCEL_DT, BN_RENEW_DT, BN_STATE_NUM, BN_STATE_OF_REG, BN_ABN;
 
-	            int heapFileSize = 0;
-	            int numOfPage = 1;
-	            int numOfRecords = 0;
-	            String fileName = "heap." + pageSize;
-	
-	            while ((line = br.readLine()) != null) {
-	            	
-	            	if (count > 0) {
-	            			String[] fileLines = line.split(cvsSplitBy);
-			                            			
-			                REGISTER_NAME = fileLines[0].getBytes();
-			                BN_NAME = fileLines[1].getBytes();
-			                BN_STATUS = fileLines[2].getBytes();
-			                BN_REG_DT = fileLines[3].getBytes();
-			                BN_CANCEL_DT = fileLines[4].getBytes();
-			                BN_RENEW_DT = fileLines[5].getBytes();
-			                
-			                try {
-			                BN_STATE_NUM = fileLines[6].getBytes();
-			                }catch (IndexOutOfBoundsException e){	
-			                	
-							}
-			                
-			                try {
-			                BN_STATE_OF_REG = fileLines[7].getBytes();
-			                }catch (IndexOutOfBoundsException e) {
-			                	
-			                }
-			                
-			                try {
-			                BN_ABN = fileLines[8].getBytes();
-			                }catch (IndexOutOfBoundsException e) {
-			                	
-			                }			                			                
-			                //System.out.println(fileLines[0] + "\t" + fileLines[1] + "\t" + fileLines[2] + "\t" + fileLines[3] + "\t" + fileLines[4] + "\t" + fileLines[5] + "\t" + fileLines[6] + "\t" + fileLines[7] + "\t" + fileLines[8]);
-			                			                
-			                for (int i=0;i<8;i++) {			                		                
-				                try {
-			                	heapFileSize = heapFileSize + fileLines[i].length();
-				                }catch (IndexOutOfBoundsException e) {
-				                	
-				                }
-			                }
-				               			    				                
-				                if (heapFileSize < pageSizeINT) {
-				                	writeFile();					                
-					                numOfRecords++;			                
-				                }
-				                else {
-				                	hFile.close();
-				                	new File(fileName).delete();
-				                	hFile = new DataOutputStream(new FileOutputStream("heap." + pageSize));
-				                	writeFile();
-					                
-					                numOfRecords++;
-					                numOfPage++;
-				                	heapFileSize = 0;	//new heapfile
-				                					                	
-				                }	
-	            	}//end of if 
-	            	else	
-	            	count = 1;
-	            }//while
-	         long stopTime = System.nanoTime();
-	         long timeUsed = (stopTime - startTime) / 1000000;
-	         System.out.println("Time used = " + timeUsed + " milliseconds");   
-	         System.out.println("Records loaded = " + numOfRecords);
-	         System.out.println("Page used = " + numOfPage);
-	        }//try 
-        
-        
-        
-        
-	        catch (FileNotFoundException e) {
-	            e.printStackTrace();
-	        } catch (IOException e) {
-	            e.printStackTrace();
-	        } finally {
-	            if (br != null) {
-	                try {
-	                    br.close();
-	                    hFile.close();
-	                } catch (IOException e) {
-	                    e.printStackTrace();
-	                }
-	            }
-	        }
-	
-	    }//if args[0]
-        else if(args[0] == null || args[1] == null || args[0] != "-p")
-        	System.out.println("Error! try --> $ java dbload -p pagesize");
-        else
-        	System.out.println("Command: $ java dbload -p pagesize");
+		int BN_NAMEsize = 200;
+		int BN_STATUSsize = 1;
+		int BN_REG_DTsize = 10;
+		int BN_CANCEL_DTsize = 10;
+		int BN_RENEW_DTsize = 10;
+		int BN_STATE_NUMsize = 10;
+		int BN_STATE_OF_REGsize = 2;
+		int BN_ABNsize = 20;
+		int recordSize = BN_NAMEsize + BN_STATUSsize + BN_REG_DTsize + BN_CANCEL_DTsize + BN_RENEW_DTsize + BN_STATE_NUMsize + BN_STATE_OF_REGsize + BN_ABNsize;
+
+		if(pageSize<recordSize){
+			System.out.println(
+				"Page size should be larger or equal to " + recordSize + 
+				"\nError.");
+			System.exit(0);
+		}
+		int recordPerPage = pageSize/recordSize;
+		int remainderPage = pageSize%recordSize;		
+		int numOfRecord = 0;
+		int numOfPage = 1;
+		long startTime = System.nanoTime();
+
+		try{
+			br = new BufferedReader(new FileReader(dataFile));
+			os = new DataOutputStream(new FileOutputStream("heap." + pageSize));
+			br.readLine();
+
+			int currRec = 0;
+			while((line = br.readLine()) != null){
+				//separate the line
+				String[] split = line.split(csvDelimiter);
+				BN_NAME = split[1];
+				BN_STATUS = split[2];
+				BN_REG_DT = split[3];
+				BN_CANCEL_DT = split[4];
+				BN_RENEW_DT = split[5];
+				try{
+					BN_STATE_NUM = split[6];
+				} catch (IndexOutOfBoundsException e){
+					BN_STATE_NUM = "";
+				}
+				try{
+					BN_STATE_OF_REG = split[7];
+				} catch (IndexOutOfBoundsException e){
+					BN_STATE_OF_REG = "";
+				}
+				try{
+					BN_ABN = split[8];
+					BN_ABN = BN_ABN.replace("\n", "");
+				} catch (IndexOutOfBoundsException e){
+					BN_ABN = "";
+				}
 
 
-    }//class
-    
-    private static void writeFile() throws IOException {
+				byte[] byteName = BN_NAME.getBytes();
+				byte[] namePad = Arrays.copyOf(byteName, BN_NAMEsize);
+				
+				boolean bolStatus = BN_STATUS.contentEquals("Registered");
 
-        hFile.write(REGISTER_NAME); hFile.write(0x09);		//0x09 = \t
-        hFile.write(BN_NAME); hFile.write(0x09);
-        hFile.write(BN_STATUS); hFile.write(0x09);
-        hFile.write(BN_REG_DT); hFile.write(0x09);
-        hFile.write(BN_CANCEL_DT); hFile.write(0x09);
-        hFile.write(BN_RENEW_DT); hFile.write(0x09);
-        
-        if (BN_STATE_NUM != null)
-        hFile.write(BN_STATE_NUM); hFile.write(0x09);
+				byte[] byteRegDate = BN_REG_DT.getBytes();
+				byte[] regDtPad = Arrays.copyOf(byteRegDate, BN_REG_DTsize);
 
-        if (BN_STATE_OF_REG != null)
-        hFile.write(BN_STATE_OF_REG); hFile.write(0x09);
-    
-        if (BN_ABN != null)
-        hFile.write(BN_ABN); hFile.write(0x0A);		//0x0A = \n
+				byte[] byteCancelDate = BN_CANCEL_DT.getBytes();
+				byte[] cancelDtPad = Arrays.copyOf(byteCancelDate, BN_CANCEL_DTsize);
+
+				byte[] byteRenewDate = BN_RENEW_DT.getBytes();
+				byte[] renewDtPad = Arrays.copyOf(byteRenewDate, BN_RENEW_DTsize);
+				
+				byte[] byteStateNum = BN_STATE_NUM.getBytes();
+				byte[] stateNumPad = Arrays.copyOf(byteStateNum, BN_STATE_NUMsize);
+				
+				short shtState = mapState.get(BN_STATE_OF_REG);
+
+				byte[] byteABN = BN_ABN.getBytes();
+				byte[] abnPad = Arrays.copyOf(byteABN, BN_ABNsize);
+
+				os.write(namePad);
+				os.writeBoolean(bolStatus);
+				os.write(regDtPad);
+				os.write(cancelDtPad);
+				os.write(renewDtPad);
+				os.write(stateNumPad);
+				os.writeShort(shtState);
+				os.write(abnPad);
+
+				currRec++;
+				numOfRecord++;
+				if(currRec == recordPerPage){
+					currRec = 0;
+					numOfPage++;
+					for(int i=0; i<remainderPage; i++){
+						os.write(0);
+					}
+				}
+			}
+			for(int i=0; i<(recordPerPage-currRec)*recordSize+remainderPage; i++){
+				os.write(0);
+			}
+		}catch (FileNotFoundException e){
+			e.printStackTrace();
+		}catch (IOException e) {
+			e.printStackTrace();
+		}finally {
+			if (br != null){
+				try{
+					br.close();
+				} catch (IOException e){
+					e.printStackTrace();
+				}
+			}
+			if (os != null){
+				try{
+					os.close();
+				} catch (IOException e){
+					e.printStackTrace();
+				}
+			}
+		}
+        long stopTime = System.nanoTime();
+        long timeUsed = (stopTime - startTime) / 1000000;
+		System.out.println("Time used = " + timeUsed + " milliseconds");
+        System.out.println("Records loaded = " + numOfRecord);
+        System.out.println("Page used = " + numOfPage);
+
 	}
 
-}//dbload
+
+	private static final Map<String, Short> mapState = new HashMap<String, Short>();
+	static{
+		mapState.put("", (short)0);
+		mapState.put("NSW", (short)1);
+		mapState.put("ACT", (short)2);
+		mapState.put("VIC", (short)3);
+		mapState.put("QLD", (short)4);
+		mapState.put("SA", (short)5);
+		mapState.put("WA", (short)6);
+		mapState.put("TAS", (short)7);
+		mapState.put("NT", (short)8);
+	}
+}
--- dbquery.java
+++ dbquery.java
@@ -1,214 +1,153 @@
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
+import java.util.*;
 
+public class dbquery{
+	static String query;
+	static int pageSize;
 
-public class dbquery {
-	
-	public static int count=0;
-	public static String pageSize, searchQuery;
-    public static byte[] REGISTER_NAME, BN_NAME, BN_STATUS, BN_REG_DT, BN_CANCEL_DT, BN_RENEW_DT, BN_STATE_NUM, BN_STATE_OF_REG, BN_ABN = null;
-    public static DataOutputStream hFile = null;
-	public static String fileName = "heap." + pageSize;
-	private static BufferedReader reader;
+	public static void main (String[] args){
 
-	
-    public static void main(String[] args) {
-    	
-        //String csvFile = "C:\\Users\\Victor Lee\\eclipse-dbs1\\DBHeapFile\\src\\BUSINESS_NAMES_201803.csv";
-        //String csvFile = "C:\\Users\\Victor Lee\\eclipse-dbs1\\DBHeapFile\\src\\testline.txt";
-    	String csvFile = "/home/ec2-user/javadbs1/BUSINESS_NAMES_201803.csv";
-        BufferedReader br = null;
-
-        String line = "";
-        String cvsSplitBy = "\t";
-        
-        try {
-        	searchQuery = args[0];
-        	pageSize = args[1];
-        	int pageSizeINT = Integer.parseInt(pageSize);       	
-        	long startTime = System.nanoTime();       	
-
-	        try {
-	
-	            br = new BufferedReader(new FileReader(csvFile));
-	            hFile = new DataOutputStream(new FileOutputStream("heap." + pageSize));
-
-	            int heapFileSize = 0;
-	            int numOfPage = 1;
-	            int numOfRecords = 0;
-
-	
-	            while ((line = br.readLine()) != null) {
-	            	
-	            	if (count > 0) {
-	            			String[] fileLines = line.split(cvsSplitBy);
-			                int lineLength = 0;           			
-			                REGISTER_NAME = fileLines[0].getBytes();
-			                BN_NAME = fileLines[1].getBytes();
-			                BN_STATUS = fileLines[2].getBytes();
-			                BN_REG_DT = fileLines[3].getBytes();
-			                BN_CANCEL_DT = fileLines[4].getBytes();
-			                BN_RENEW_DT = fileLines[5].getBytes();
-			                
-			                try {
-			                BN_STATE_NUM = fileLines[6].getBytes();
-			                }catch (IndexOutOfBoundsException e){	
-			                	
-							}
-			                
-			                try {
-			                BN_STATE_OF_REG = fileLines[7].getBytes();
-			                }catch (IndexOutOfBoundsException e) {
-			                	
-			                }
-			                
-			                try {
-			                BN_ABN = fileLines[8].getBytes();
-			                }catch (IndexOutOfBoundsException e) {
-			                	
-			                }			                
-			                
-			                for (int i=0;i<8;i++) {			                		                
-				                try {
-			                	heapFileSize = heapFileSize + fileLines[i].length();
-			                	lineLength = lineLength + fileLines[i].length();
-				                }catch (IndexOutOfBoundsException e) {
-				                	
-				                }
-			                }
-				               			    
-				                
-				                if (heapFileSize + lineLength < pageSizeINT) {
-				                	writeFile();					                
-					                numOfRecords++;			                
-				                }
-				                else {				                	
-				                	hFile.close();
-				                	scanFiles();		//scan heap.pagesize for query
-				                	new File(fileName).delete();			                	
-				                	hFile = new DataOutputStream(new FileOutputStream("heap." + pageSize));
-				                	writeFile();					                
-					                numOfRecords++;
-					                numOfPage++;
-				                	heapFileSize = lineLength;	//new heapfile
-				                	
-				                	
-				                }	
-	            	}//end of if 
-	            	else	
-	            	count = 1;
-	            }//while
-	         long stopTime = System.nanoTime();
-	         long timeUsed = (stopTime - startTime) / 1000000;
-	         System.out.println("Time used = " + timeUsed + " milliseconds");   
-	         System.out.println("Records loaded = " + numOfRecords);
-	         System.out.println("Page used = " + numOfPage);
-	        }//try 
-        
-        
-        
-        
-	        catch (FileNotFoundException e) {
-	            e.printStackTrace();
-	        } catch (IOException e) {
-	            e.printStackTrace();
-	        } finally {
-	            if (br != null) {
-	                try {
-	                    br.close();
-	                    hFile.close();
-	                } catch (IOException e) {
-	                    e.printStackTrace();
-	                }
-	            }
-	        }
-	
-	    }//try args0
-        catch (IndexOutOfBoundsException e){
-        	System.out.println("Error! try --> $ java dbquery BN_NAME pagesize");
-        }
-    }//class
-    
-    private static void scanFiles() throws IOException {
-    	reader = new BufferedReader(new FileReader("heap." + pageSize));
-        String line = "";
-        String cvsSplitBy = "\t";            
-        
-        while ((line = reader.readLine()) != null) {
-	    	String[] token = line.split(cvsSplitBy);
-	    	String tokenText0, tokenText1, tokenText2, tokenText3, tokenText4, tokenText5, tokenText6 = null, tokenText7 = null, tokenText8 = null;
-	    	byte[] token0, token1, token2, token3, token4, token5, token6 = null, token7 = null, token8 = null;
-	    	boolean boo6=false, boo7=false, boo8=false;
-	    	
-	        token0 = token[0].getBytes();
-	        token1 = token[1].getBytes();
-	        token2 = token[2].getBytes();
-	        token3 = token[3].getBytes();
-	        token4 = token[4].getBytes();
-	        token5 = token[5].getBytes();        
-	        try {
-	        	token6 = token[6].getBytes();
-	        }catch (IndexOutOfBoundsException e){	 
-	        	boo6=true;
-			}        
-	        try {
-	        	token7 = token[7].getBytes();
-	        }catch (IndexOutOfBoundsException e) {   
-	        	boo7=true;
-	        }        
-	        try {
-	        	token8 = token[8].getBytes();
-	        }catch (IndexOutOfBoundsException e) { 
-	        	boo8=true;
-	        }
-	        
-	        tokenText0 = new String(token0, "UTF-8");
-	        tokenText1 = new String(token1, "UTF-8");
-	        tokenText2 = new String(token2, "UTF-8");
-	        tokenText3 = new String(token3, "UTF-8");
-	        tokenText4 = new String(token4, "UTF-8");
-	        tokenText5 = new String(token5, "UTF-8");	
-	        if (boo6==false)
-	        	tokenText6 = new String(token6, "UTF-8");
-	        if (boo7==false)
-	        	tokenText7 = new String(token7, "UTF-8");
-	        if (boo8==false)
-	        	tokenText8 = new String(token8, "UTF-8");
-	        
-	        if(tokenText1.toLowerCase().contains(searchQuery.toLowerCase()))
-        	    System.out.println(tokenText0 + "\t" + tokenText1 + "\t" + tokenText2 + "\t" + tokenText3 + "\t" + tokenText4 + "\t" + tokenText5 + "\t" + tokenText6 + "\t" + tokenText7 + "\t" + tokenText8);
-        }
+		if(args.length == 2){
+			query = args[0];
+			String strPage = args[1];
+			try{
+				pageSize = Integer.parseInt(strPage);
+			} catch (IndexOutOfBoundsException e){
+				System.out.println("Page size must be an integer.");
+				System.exit(0);
+			}
+		}else{
+			System.out.println("Error!.");
+			System.exit(0);
+		}
+		long startTime = System.nanoTime();
+		int BN_NAMEsize = 200;
+		int BN_STATUSsize = 1;
+		int BN_REG_DTsize = 10;
+		int BN_CANCEL_DTsize = 10;
+		int BN_RENEW_DTsize = 10;
+		int BN_STATE_NUMsize = 10;
+		int BN_STATE_OF_REGsize = 2;
+		int BN_ABNsize = 20;
+		int recordSize = BN_NAMEsize + BN_STATUSsize + BN_REG_DTsize + BN_CANCEL_DTsize + BN_RENEW_DTsize + BN_STATE_NUMsize + BN_STATE_OF_REGsize + BN_ABNsize;
 		
+		int pageOffset = 0;
+		int recordPerPage = pageSize/recordSize;
+		byte[] emptyByte = new byte[200];
+		int currRec = 0;
+		
+		RandomAccessFile in = null;
+		try{
+			startTime = System.nanoTime();
+			in = new RandomAccessFile("heap."+pageSize, "r");
+			while(true){
+
+				int currOffset = currRec * recordSize + pageOffset * pageSize;
+				in.seek(currOffset);
+				byte[] readByte = new byte[BN_NAMEsize];			
+				in.read(readByte);
+				String value1 = new String(readByte, "UTF-8");
+
+				if(value1.toLowerCase().contains(query.toLowerCase())){
+
+					String bn_name;
+					String bn_status;
+					String bn_reg_dt;
+					String bn_cancel_dt;
+					String bn_renew_dt;
+					String bn_state_num;
+					String bn_state_of_reg;
+					String bn_abn;
+
+					bn_name = value1;
+
+					in.seek(currOffset + BN_NAMEsize);
+					boolean bolStatus = in.readBoolean();
+					if(bolStatus)
+						bn_status = "Registered";
+					else
+						bn_status = "Deregistered";
+
+					in.seek(currOffset + BN_NAMEsize + BN_STATUSsize);
+					byte[] readRegDate = new byte[BN_REG_DTsize];
+					in.read(readRegDate);
+					bn_reg_dt = new String(readRegDate);
+					
+					in.seek(currOffset + BN_NAMEsize + BN_STATUSsize + BN_REG_DTsize);
+					byte[] readCancelDate = new byte[BN_CANCEL_DTsize];
+					in.read(readCancelDate);
+					bn_cancel_dt = new String(readCancelDate);
+
+					in.seek(
+						currOffset + BN_NAMEsize + BN_STATUSsize + 
+						BN_REG_DTsize + BN_CANCEL_DTsize);
+					byte[] readRenewDate = new byte[BN_RENEW_DTsize];
+					in.read(readRenewDate);
+					bn_renew_dt = new String(readRenewDate);
+
+					in.seek(
+						currOffset + BN_NAMEsize + BN_STATUSsize + 
+						BN_REG_DTsize + BN_CANCEL_DTsize + BN_RENEW_DTsize);
+					byte[] readStateNum = new byte[BN_STATE_NUMsize];
+					in.read(readStateNum);
+					bn_state_num = new String(readStateNum);
+
+					in.seek(
+						currOffset + BN_NAMEsize + BN_STATUSsize + 
+						BN_REG_DTsize + BN_CANCEL_DTsize + BN_RENEW_DTsize +
+						BN_STATE_NUMsize);
+					short shtState = in.readShort();
+					bn_state_of_reg = mapState.get(shtState);
+					
+					in.seek(
+						currOffset + BN_NAMEsize + BN_STATUSsize + 
+						BN_REG_DTsize + BN_CANCEL_DTsize + BN_RENEW_DTsize +
+						BN_STATE_NUMsize + BN_STATE_OF_REGsize);
+					byte[] readABN = new byte[BN_ABNsize];
+					in.read(readABN);
+					bn_abn = new String(readABN);
+
+					System.out.println(bn_name + "\t" + bn_status + "\t" + bn_reg_dt + "\t" + bn_cancel_dt + "\t" + 
+							bn_renew_dt + "\t" + bn_state_num + "\t" + bn_state_of_reg + "\t" + bn_abn + "\t");
+				}
+
+				currRec++;
+				if(currRec == recordPerPage){
+					currRec = 0;
+					pageOffset++;
+				}
+				if(Arrays.equals(readByte, emptyByte)){
+			        long stopTime = System.nanoTime();
+			        long timeUsed = (stopTime - startTime) / 1000000;
+					System.out.println("Time used = " + timeUsed + " milliseconds");
+					break;
+				}
+			}
+		}catch (FileNotFoundException e){
+			e.printStackTrace();
+		}catch (IOException e) {
+			e.printStackTrace();
+		}finally {
+			if (in != null){
+				try{
+					in.close();
+				} catch (IOException e){
+					e.printStackTrace();
+				}
+			}
+		}
 	}
 
-
-
-	private static void writeFile() throws IOException {
-
-        hFile.write(REGISTER_NAME); hFile.write(0x09);		//0x09 = \t
-        hFile.write(BN_NAME); hFile.write(0x09);
-        hFile.write(BN_STATUS); hFile.write(0x09);
-        hFile.write(BN_REG_DT); hFile.write(0x09);
-        hFile.write(BN_CANCEL_DT); hFile.write(0x09);
-        hFile.write(BN_RENEW_DT); hFile.write(0x09);
-        
-        if (BN_STATE_NUM != null)
-        hFile.write(BN_STATE_NUM); hFile.write(0x09);
-
-        if (BN_STATE_OF_REG != null)
-        hFile.write(BN_STATE_OF_REG); hFile.write(0x09);
-    
-        if (BN_ABN != null)
-        hFile.write(BN_ABN); hFile.write(0x0A);		//0x0A = \n
+	private static final Map<Short, String> mapState = new HashMap<Short, String>();
+	static{
+		mapState.put((short)0, "");
+		mapState.put((short)1, "NSW");
+		mapState.put((short)2, "ACT");
+		mapState.put((short)3, "VIC");
+		mapState.put((short)4, "QLD");
+		mapState.put((short)5, "SA");
+		mapState.put((short)6, "WA");
+		mapState.put((short)7, "TAS");
+		mapState.put((short)8, "NT");
 	}
-
-    
-    
-}//dbload
-
-
+}
